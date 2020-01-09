b4d0f4a2f759e07da6d3758b0c27d1d0ce431274
Angel Plascencia <aplascencia@nearsoft.com>
2020-01-08 18:25:07 -0600

    Re-creating config file

--- ssh_forwarding.sh
+++ ssh_forwarding.sh
@@ -1,53 +1,115 @@
 #!/bin/bash
 
-# Getting the Bastion 
+namespace="$1"
+file_config_personal="/tmp/config_personal"
+file_config_temp="/tmp/config"
+file_config_local="${HOME}/.ssh/config"
+
+################################################
+# Validate if exists file names and remove them
+################################################
+if [[ -e "/tmp/config_$namespace"  ]]; then
+  echo "File ${namespace} already exists"
+  
+  rm "/tmp/config_$namespace"
+  
+  echo "File removed /tmp/config_$namespace"
+#   exit 1
+fi
+
+if [[ -e ${file_config_personal}  ]]; then
+  echo "File ${file_config_personal} already exists"
+  
+  rm "${file_config_personal}"
+  
+  echo "File removed ${file_config_personal}"
+#   exit 1
+fi
+
+if [[ -e ${file_config_temp}  ]]; then
+  echo "File ${file_config_temp} already exists"
+  
+  rm "${file_config_temp}"
+  
+  echo "File removed ${file_config_temp}"
+#   exit 1
+fi
+
+# Just for testint
+# cp /tmp/config_copy /tmp/config
+
+
+########################
+# Getting info from AWS
+########################
+# Bastion info
 aws ec2 describe-instances \
 --filters "Name=owner-id,Values=738397695583" "Name=instance-state-name,Values=running" "Name=tag-value,Values=*_bastion" \
 --query 'Reservations[*].Instances[*].{imageId:ImageId,publicIp:PublicIpAddress,privateIp:PrivateIpAddress}' \
 --output json > /tmp/aws_bastion.json
 
-# Getting the private instances
+# Private instances info
 aws ec2 describe-instances \
 --filters "Name=owner-id,Values=738397695583" "Name=instance-state-name,Values=running" "Name=tag-value,Values=*_private" \
 --query 'Reservations[*].Instances[*].{imageId:ImageId,publicIp:PublicIpAddress,privateIp:PrivateIpAddress}' \
 --output json > /tmp/aws_privates.json
 
+
+# Copy your local config file to temp config personal for re-create it
+cat "${file_config_local}" > "${file_config_personal}"
+# cat ~/.ssh/config > /tmp/config_personal
+
+
 # Set variables like json
 bastion=$(cat /tmp/aws_bastion.json)
 privates=$(cat /tmp/aws_privates.json)
 
-# Init loop in order to create the config inside the config file
-for row in $(echo "${privates}" | jq -r '.[][] | @base64'); do
+
+# Init loop in order to create the config namespace file
+for row in $(echo "${bastion}" | jq -r '.[][] | @base64'); do
     _jq() {
-     echo ${row} | base64 --decode | jq -r ${1}
+    echo ${row} | base64 --decode | jq -r ${1}
     }
 
-    ip=$(_jq '.privateIp')
-    # echo $ip
+    public_ip=$(_jq '.publicIp')
 
-cat >> /tmp/config <<EOF
-Host server_${ip}
-   HostName $ip
-   User ubuntu
-   ForwardAgent yes
-   IdentityFile ~/.ssh/private_instance
-EOF
-
-    for row in $(echo "${bastion}" | jq -r '.[][] | @base64'); do
+    for row in $(echo "${privates}" | jq -r '.[][] | @base64'); do
         _jq() {
         echo ${row} | base64 --decode | jq -r ${1}
         }
 
-        ip=$(_jq '.publicIp')
-        # echo $ip
+        private_ip=$(_jq '.privateIp')
+        # host="${private_ip#.*.*.}"
+        # echo ${private_ip#*.*.*.}
+        host=${private_ip#*.*.*.}
+        
 
-cat >> /tmp/config <<EOF
-   ProxyCommand ssh ubuntu@${ip} -W %h:%p
+cat >> "/tmp/config_${namespace}" <<EOF
+Host server_${public_ip}_${host}
+   HostName $private_ip
+   User ubuntu
+   ForwardAgent yes
+   IdentityFile ${HOME}/.ssh/private_instance
+   ProxyCommand ssh ubuntu@${public_ip} -W %h:%p
 
 EOF
-    done
+    
+    done # end for ${privates}
 
-done
+done # end for ${bastion}
 
-# Add the servers in the config file
-cat /tmp/config >> ~/.ssh/config
+
+##########################
+# Re-Creating config file
+##########################
+cat ${file_config_personal} > ${file_config_temp}
+cat "/tmp/config_$namespace" >> ${file_config_temp}
+# code "${file_config_temp}"
+
+
+##########################
+# Copy result in your local config file
+##########################
+cat ${file_config_temp} > "${file_config_local}" 
+
+code "${file_config_local}" 
